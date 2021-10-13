c9cd2a81bc5b246634ba9aad26c480fdb0b2057e
Tyler Whitehouse <twhitehouse@hawkmail.hccfl.edu>
2021-10-13 09:27:51 -0400

    finished music commands

--- play.js
+++ play.js
@@ -48,18 +48,77 @@ module.exports =
                     message.channel.send('Cant find video')
                 }
             }
-        }
-        if (!server_queue)
+            if (!server_queue)
         {
             const queue_constructor = {
                 voice_channel: voice_channel,
                 text_channel: message.channel,
                 connection: null,
-                song: []
+                songs: []
             }
 
             queue.set(message.guild.id, queue_constructor);
             queue_constructor.songs.push(song);
+
+            try
+            {
+                const connection = await voice_channel.join();
+                queue_constructor.connection = connection;
+                video_player(message.guild, queue_constructor.songs[0]);
+            }
+            catch(err) 
+            {
+                queue.delete(message.guild.id);
+                message.channel.send('Error connecting to voice channel.');
+                throw err;
+            }
+
         }
+        else
+        {
+            server_queue.songs.push(song);
+            return message.channel.send('**${song.title}** added to queue.');
+        }
+        }
+        
+        else if (cmd === 'skip') skip_song(message, server_queue);
+        else if (cmd === 'stop') stop_song(message, server_queue);
     }
+}
+
+const video_player = async (guild, song) =>
+{
+    const song_queue = queue.get(guild.id);
+
+    if(!song)
+    {
+        song_queue.voice_channel.leave();
+        queue.delete(guild.id);
+        return;
+
+    }
+    const stream = ytdl(song.url, {filter: 'audioonly'});
+    song_queue.connection.play(stream, {seek: 0, volume: 0.5}).on('finish', () =>
+    {
+        song_queue.songs.shift();
+        video_player(guild, song_queue.songs[0]);
+    });
+    await song_queue.text_channel.send('Now playing **${song.title}**')
+}
+
+const skip_song = (message, server_queue) =>
+{
+    if (!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to do this.');
+    if (!server_queue) 
+    {
+        return message.channel.send('No songs in queue.')
+    }
+    server_queue.connection.dispatcher.end();
+}
+
+const stop_song = (message, server_queue) =>
+{
+    if (!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to do this.');
+    server_queue.songs = [];
+    server_queue.connection.dispatcher.end();
 }
